1908daec9df9ce6999f6653e7600f645d49c67b3
rodolfojsilva <rodolfo0ti@gmail.com>
2022-06-19 01:45:56 -0300

    Adicionar arquivos de projeto.

---
+++ todo-manager/Profiles/TodoProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using todo_manager.Models.Data.Dtos;
+using todo_manager.Models.Entitie;
+
+namespace todo_manager.Profiles
+{
+    public class TodoProfile : Profile
+    {
+        public TodoProfile()
+        {
+            CreateMap<CreateCardDto, Todo>();
+        }
+
+    }
+}
